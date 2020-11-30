e35b3114879478aa919796a71c049de8f1a91529
Joshua Mclean <joshmcleana@gmail.com>
2020-11-29 16:13:03 -0800

    Make gear.rb

---
+++ bicycle_gear_ratio/gear.rb
@@ -0,0 +1,12 @@
+# chainring = 52  #number of teeth
+# cog = 11
+# ratio = chainring / cog.to_f
+# puts ratio  #=> 4.7272727272727275, each time the pedals complete one rotation, the wheels turn almost 5 times, harder gear
+
+# chainring2 = 30  #number of teeth
+# cog2 = 27
+# ratio2 = chainring2 / cog2.to_f
+# puts ratio2 #=> 1.1111111111111112, each time the pedals turn once, the wheel turns about 1 time, easy gear
+
+
+
